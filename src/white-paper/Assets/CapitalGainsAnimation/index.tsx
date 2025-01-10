918a26e1efb39439c3608af5e3a91662d4efdd8d
Karthik Uppu <kartuppuluri@gmail.com>
2025-01-10 13:44:35 +0530

    .

--- src/white-paper/Assets/CapitalGainsAnimation/index.tsx
+++ src/white-paper/Assets/CapitalGainsAnimation/index.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { FC, useEffect, useState } from "react";
+import { FC, useState } from "react";
 import { PersonalTokenComp } from "../PersonalToken";
 import { dummyPersonalToken, PersonalToken } from "../PersonalToken/models";
 import { Card } from "../Card";
@@ -10,6 +10,7 @@ import styles from "./CapitalGains.module.css";
 
 export const CapitalGainsAnimation: FC = ({}) => {
   const [token, setToken] = useState<PersonalToken>(dummyPersonalToken);
+  const [key, setKey] = useState(0);
 
   const animate = () => {
     // Remove the first portfolio item if it exists
@@ -22,11 +23,13 @@ export const CapitalGainsAnimation: FC = ({}) => {
       return;
     } else {
       setToken(dummyPersonalToken);
+      setKey((prev) => prev + 1); // Force re-mount of component
     }
   };
 
   return (
     <Card
+      key={key}
       caption={
         "When assets are sold, capital gains are distributed to shareholders proportional to the equity they hold."
       }
